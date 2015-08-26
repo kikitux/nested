d3a8ebf2fb97f2ad38233ca3faf25f6a0ea8f8be
Alvaro Miranda <kikitux@gmail.com>
2015-08-26 21:08:46 +1200

    add test to nodejs app

--- test/app.js
+++ test/app.js
@@ -8,5 +8,5 @@ assert(2+2==4, '2+2=4');
 assert(factorial.fact(5)==120, 'factorial of 5 = 120');
 assert(factorial.fact(6)==720, 'factorial of 6 = 720');
 
-assert(102334155==fibonacci.fibo(40), 'fibo of 40');
-assert(267914296==fibonacci.fibo(42), 'fibo of 42');
+assert(102334155==fibonacci.fibo(40), 'fibo(40)');
+assert(267914296==fibonacci.fibo(42), 'fibo(42)');
